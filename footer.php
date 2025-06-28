3db45c10ffa69584f01d37d104d1da908e5ee138
zakawaki <148469407+zakawaki@users.noreply.github.com>
2025-06-27 22:58:48 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="bg-dark text-white text-center py-3 mt-5">
+  <p>© 2025 José Odalis Encarnación Rivera</p>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Portal de APIs</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="estilos.css?v=<?php echo time(); ?>">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="../index.php">Inicio</a>
+    <button class="navbar-toggler" ...></button>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="../apis/genero.php">Género</a></li>
+        <li class="nav-item"><a class="nav-link" href="../apis/edad.php">Edad</a></li>
+        <li class="nav-item"><a class="nav-link" href="../apis/universidades.php">Universidades</a></li>
+        <li class="nav-item"><a class="nav-link" href="../apis/clima.php">Clima</a></li>
+        <li class="nav-item"><a class="nav-link" href="../apis/pokemon.php">Pokémon</a></li>
+        <li class="nav-item"><a class="nav-link" href="../apis/noticias.php">Noticias</a></li>
+        <li class="nav-item"><a class="nav-link" href="../apis/monedas.php">Monedas</a></li>
+        <li class="nav-item"><a class="nav-link" href="../apis/imagenes.php">Imágenes IA</a></li>
+        <li class="nav-item"><a class="nav-link" href="../apis/pais.php">Datos País</a></li>
+        <li class="nav-item"><a class="nav-link" href="../apis/chistes.php">Chistes</a></li>
+        <li class="nav-item"><a class="nav-link" href="../about.php">Acerca de</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
